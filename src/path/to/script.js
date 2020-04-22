eedbe99635be3c8084e28fb74222f07e7aeaaa45
SirGoPadawan <orangehardcore0@gmail.com>
2020-04-23 01:26:18 +1000

    newRepKKCO

---
+++ src/path/to/script.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", function () {
+  document.getElementById("headerSocialBtn").onclick = function () {
+    updateState("headerSocialLinks");
+  };
+  document.getElementById("headerMapBtn").onclick = function () {
+    updateState("headerMapMenu");
+  };
+  document.getElementById("headerSearchBtn").onclick = function () {
+    updateState("headerSearchPlace");
+  };
+});
+function updateState(id) {
+  let elem = document.getElementById(id);
+
+  if (elem.classList.contains("active")) {
+    elem.classList.remove("active");
+  } else elem.classList.add("active");
+}
+
+/* function menuOpen(id) {
+  let n = document.querySelectorAll(".nav-middle__btn");
+  n.forEach(function (btn) {
+    btn.onclick = function () {
+      n.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
+      btn.classList.add("active");
+    };
+  });
+}
+ */
